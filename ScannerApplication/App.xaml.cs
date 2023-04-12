8539534766c145d566e379cef0895c8c990fc06c
Ahmed Razzaq <ahrazzaq@earthlink.iq>
2023-04-12 12:11:57 +0300

    parsing argument as real estate id

--- ScannerApplication/App.xaml.cs
+++ ScannerApplication/App.xaml.cs
@@ -14,15 +14,55 @@ namespace ScannerApplication
     {
         public App()
         {
-           this.InitializeComponent();
+            this.InitializeComponent();
         }
         private void App_OnStartup(object sender, StartupEventArgs e)
         {
-            
-            string[] args = e.Args;
-            var realEstateId = args.Length > 0 ? Guid.Parse(args[0]) : Guid.Empty;
-            MainWindow mainWindow = new MainWindow(realEstateId);
-            mainWindow.Show();
+            if (e.Args.Length > 0)
+            {
+
+                // Pass the string as an argument to the application
+                string inputString = e.Args[0];
+
+                // Extract the GUID from the input string
+                string guidString = ExtractGuidFromInputString(inputString);
+                // Attempt to parse the extracted GUID as a Guid
+                if (Guid.TryParse(guidString, out Guid realEstateId))
+                {
+                    MainWindow mainWindow = new MainWindow(realEstateId);
+                    mainWindow.Show();
+                }
+                else
+                {
+                    MessageBox.Show("Invalid GUID format.");
+                    Current.Shutdown();
+                }
+            }
+            MessageBox.Show("No arguments.");
+            Current.Shutdown();
+
+        }
+
+        static string ExtractGuidFromInputString(string inputString)
+        {
+            // Split the input string by the colon (":") character
+            string[] splitString = inputString.Split(':');
+
+            // The GUID should be the second element in the splitString array
+            // after the "scanner" prefix
+            if (splitString.Length > 1)
+            {
+                // Extract the GUID from the second element
+                string guid = splitString[1];
+
+                // Return the extracted GUID
+                return guid;
+            }
+            else
+            {
+                // If the input string is not in the expected format, return an empty string
+                return string.Empty;
+            }
         }
     }
 }
